8450dbb34b5a7cc6d2f40a22ab23457195dd1bb9
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-07 21:28:11 +0300

    Added fields to UserConfig

--- telegram/src/main/java/dev/tishenko/restobot/telegram/config/UserConfig.java
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/config/UserConfig.java
@@ -1,5 +1,21 @@
 package dev.tishenko.restobot.telegram.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class UserConfig
-{
+import java.util.List;
+
+
+@Configuration
+public class UserConfig {
+    private final int chatID;
+    private final String nickName;
+    private final int favoriteListId;
+    private String city;
+    private List<String> keyWords;
+
+    public UserConfig(int chatID, String nickName, int favoriteListId) {
+        this.chatID = chatID;
+        this.nickName = nickName;
+        this.favoriteListId = favoriteListId;
+    }
 }
