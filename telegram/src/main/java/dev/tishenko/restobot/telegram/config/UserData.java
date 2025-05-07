e041701738d4c44d3930513b61f8090568c874a6
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-07 21:50:13 +0300

    Renamed UserConfig to UserData

---
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/config/UserData.java
@@ -0,0 +1,15 @@
+package dev.tishenko.restobot.telegram.config;
+
+import java.util.List;
+
+
+public class UserData {
+    private int chatID;
+    private String nickName;
+    private int favoriteListId;
+    private String city;
+    private List<String> keyWords;
+
+    public UserData() {
+    }
+}
