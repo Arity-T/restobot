70e6ca840bc2d34653b58df6103f6176ddb8563c
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-07 23:07:24 +0300

    Added class RestaurantCard with fields

---
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/config/RestaurantCard.java
@@ -0,0 +1,102 @@
+package dev.tishenko.restobot.telegram.config;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+
+
+@Component
+@Scope("prototype")
+public class RestaurantCard {
+    private int restaurantId;
+    private int tripadvisorId;
+    private String name;
+    private String addressString;
+    private double rating;
+    private URL website;
+    private String description;
+    private double latitude;
+    private double longitude;
+    private String city;
+
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public int getTripadvisorId() {
+        return tripadvisorId;
+    }
+
+    public void setTripadvisorId(int tripadvisorId) {
+        this.tripadvisorId = tripadvisorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressString() {
+        return addressString;
+    }
+
+    public void setAddressString(String addressString) {
+        this.addressString = addressString;
+    }
+
+    public URL getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(URL website) {
+        this.website = website;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
