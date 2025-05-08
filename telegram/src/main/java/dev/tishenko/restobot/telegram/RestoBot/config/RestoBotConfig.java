913a49bd8bc095285ca1193dd037982a756b996d
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-08 17:46:14 +0300

    Added class RestoBotConfig

---
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/RestoBot/config/RestoBotConfig.java
@@ -0,0 +1,13 @@
+package dev.tishenko.restobot.telegram.RestoBot.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+
+@Configuration
+@Scope("singleton")
+public class RestoBotConfig {
+
+
+
+}
