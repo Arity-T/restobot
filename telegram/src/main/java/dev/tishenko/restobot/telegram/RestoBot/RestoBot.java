23921b8809315642d26cb11fa94222f990f99bab
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-08 17:18:01 +0300

    Added fields and constructor to RestoBot

--- telegram/src/main/java/dev/tishenko/restobot/telegram/RestoBot/RestoBot.java
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/RestoBot/RestoBot.java
@@ -2,10 +2,21 @@ package dev.tishenko.restobot.telegram.RestoBot;
 
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class RestoBot implements LongPollingUpdateConsumer {
+    private Executor updatesProcessorExecutor = Executors.newVirtualThreadPerTaskExecutor();
+    private final TelegramClient telegramClient;
+
+    public RestoBot(TelegramClient telegramClient) {
+        this.telegramClient = telegramClient;
+    }
+
+
     @Override
     public void consume(List<Update> updates) {
 
