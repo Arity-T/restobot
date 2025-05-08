4bcdde7f740ba22b41486b0aeedad87075b00fdb
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-08 17:16:47 +0300

    Added RestoBot class

---
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/RestoBot/RestoBot.java
@@ -0,0 +1,13 @@
+package dev.tishenko.restobot.telegram.RestoBot;
+
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+public class RestoBot implements LongPollingUpdateConsumer {
+    @Override
+    public void consume(List<Update> updates) {
+
+    }
+}
