2f1546327f4fdd2e1a57816d70fffd7cc7a8fefc
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-13 22:03:00 +0300

    Добавил UserService

---
+++ logic/src/main/java/dev/tishenko/restobot/logic/service/UserService.java
@@ -0,0 +1,26 @@
+package dev.tishenko.restobot.logic.service;
+
+import java.util.Optional;
+
+import org.example.jooq.generated.tables.records.UsersRecord;
+import org.springframework.stereotype.Service;
+
+import dev.tishenko.restobot.logic.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository repo) {
+        this.userRepository = repo;
+    }
+
+    public void registerUser(long chatId, String nickname) {
+        userRepository.saveUser(chatId, nickname);
+    }
+
+    public Optional<UsersRecord> getUserById(long chatId) {
+        return Optional.ofNullable(userRepository.findByChatId(chatId));
+    }
+}
