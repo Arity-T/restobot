fe6b7a84d6b6085272f4d4f7779280e9ce515196
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-13 22:13:06 +0300

    Добавил CityRepository

---
+++ logic/src/main/java/dev/tishenko/restobot/logic/repository/CityRepository.java
@@ -0,0 +1,33 @@
+package dev.tishenko.restobot.logic.repository;
+
+import java.util.List;
+
+import org.example.jooq.generated.tables.City;
+import org.example.jooq.generated.tables.records.CityRecord;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CityRepository {
+    private final DSLContext dsl;
+
+    public CityRepository(DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    public CityRecord findById(int cityId) {
+        return dsl.selectFrom(City.CITY)
+                  .where(City.CITY.CITY_ID.eq(cityId))
+                  .fetchOne(); // вернёт null, если не найден
+    }
+
+    public List<CityRecord> findAll() {
+        return dsl.selectFrom(City.CITY).fetch();
+    }
+
+    public CityRecord findByName(String name) {
+        return dsl.selectFrom(City.CITY)
+                  .where(City.CITY.NAME.eq(name))
+                  .fetchOne(); // вернёт null, если не найден
+    }
+}
