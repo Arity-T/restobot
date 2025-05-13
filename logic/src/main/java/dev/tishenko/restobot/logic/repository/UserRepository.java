0f1b88b8526da6a4ea743449fe3030737edcb45c
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-13 22:02:51 +0300

    Добавил UserRepository

---
+++ logic/src/main/java/dev/tishenko/restobot/logic/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package dev.tishenko.restobot.logic.repository;
+
+import org.example.jooq.generated.tables.Users;
+import org.example.jooq.generated.tables.records.UsersRecord;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    private final DSLContext dsl;
+
+    // через конструктор получаем DSLContext — он будет создан в JooqConfig
+    public UserRepository(DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    // Метод добавления пользователя
+    public void saveUser(long chatId, String nickname) {
+        dsl.insertInto(Users.USERS)
+           .set(Users.USERS.CHAT_ID, chatId)
+           .set(Users.USERS.NICKNAME, nickname)
+           .onConflictDoNothing() // чтобы не падало, если уже есть
+           .execute();
+    }
+
+    // Метод чтения пользователя
+    public UsersRecord findByChatId(long chatId) {
+        return dsl.selectFrom(Users.USERS)
+                  .where(Users.USERS.CHAT_ID.eq(chatId))
+                  .fetchOne(); // вернёт null, если не найден
+    }
+}
