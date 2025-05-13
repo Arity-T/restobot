c0cc80475b0419a30f7e649ec533d642dfe002a6
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-13 22:31:53 +0300

    Добавил FavoriteListService

---
+++ logic/src/main/java/dev/tishenko/restobot/logic/service/FavoriteListService.java
@@ -0,0 +1,38 @@
+package dev.tishenko.restobot.logic.service;
+
+import java.util.List;
+
+import org.example.jooq.generated.tables.records.FavoriteListRecord;
+import org.springframework.stereotype.Service;
+
+import dev.tishenko.restobot.logic.repository.FavoriteListRepository;
+
+@Service
+public class FavoriteListService {
+
+    private final FavoriteListRepository repo;
+
+    public FavoriteListService(FavoriteListRepository repo) {
+        this.repo = repo;
+    }
+
+    public FavoriteListRecord addRestaurant(int tripadvisorId) {
+        return repo.addRestaurant(tripadvisorId);
+    }
+
+    public void setVisited(int favoriteListId, boolean visited) {
+        repo.markVisited(favoriteListId, visited);
+    }
+
+    public void removeFromFavorites(int favoriteListId) {
+        repo.delete(favoriteListId);
+    }
+
+    public List<FavoriteListRecord> getAll() {
+        return repo.getAllFavorites();
+    }
+
+    public List<FavoriteListRecord> getByVisitStatus(boolean visited) {
+        return repo.getVisited(visited);
+    }
+}
