12316e387f403e3ce89d27b5a66b83e39b637391
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-13 22:48:55 +0300

    Добавил UserPriceCategoryRepository

---
+++ logic/src/main/java/dev/tishenko/restobot/logic/repository/UserPriceCategoryRepository.java
@@ -0,0 +1,38 @@
+package dev.tishenko.restobot.logic.repository;
+
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+import org.example.jooq.generated.tables.UserPriceCategory;
+import org.example.jooq.generated.tables.records.UserPriceCategoryRecord;
+
+import java.util.List;
+
+@Repository
+public class UserPriceCategoryRepository {
+
+    private final DSLContext dsl;
+
+    public UserPriceCategoryRepository(DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    public void addPriceCategory(long chatId, int categoryId) {
+        dsl.insertInto(UserPriceCategory.USER_PRICE_CATEGORY)
+           .set(UserPriceCategory.USER_PRICE_CATEGORY.CHAT_ID, chatId)
+           .set(UserPriceCategory.USER_PRICE_CATEGORY.PRICE_CATEGORY_ID, categoryId)
+           .execute();
+    }
+
+    public void removePriceCategory(long chatId, int categoryId) {
+        dsl.deleteFrom(UserPriceCategory.USER_PRICE_CATEGORY)
+           .where(UserPriceCategory.USER_PRICE_CATEGORY.CHAT_ID.eq(chatId))
+           .and(UserPriceCategory.USER_PRICE_CATEGORY.PRICE_CATEGORY_ID.eq(categoryId))
+           .execute();
+    }
+
+    public List<UserPriceCategoryRecord> getAllByUser(long chatId) {
+        return dsl.selectFrom(UserPriceCategory.USER_PRICE_CATEGORY)
+                  .where(UserPriceCategory.USER_PRICE_CATEGORY.CHAT_ID.eq(chatId))
+                  .fetch();
+    }
+}
