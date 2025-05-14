b96eb89f42d1470606169e3c0621b9539fe2e7fd
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-14 16:57:09 +0300

    Добавил PriceCategoryRepository

---
+++ data/src/main/java/dev/tishenko/restobot/data/repository/PriceCategoryRepository.java
@@ -0,0 +1,35 @@
+package dev.tishenko.restobot.data.repository;
+
+import java.util.List;
+
+import org.example.jooq.generated.tables.PriceCategory;
+import org.example.jooq.generated.tables.records.PriceCategoryRecord;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PriceCategoryRepository {
+
+    private final DSLContext dsl;
+
+    public PriceCategoryRepository(DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    public PriceCategoryRecord findById(int id) {
+        return dsl.selectFrom(PriceCategory.PRICE_CATEGORY)
+                  .where(PriceCategory.PRICE_CATEGORY.PRICE_CATEGORY_ID.eq(id))
+                  .fetchOne();
+    }
+
+    public PriceCategoryRecord findByName(String name) {
+        return dsl.selectFrom(PriceCategory.PRICE_CATEGORY)
+                  .where(PriceCategory.PRICE_CATEGORY.NAME.eq(name))
+                  .fetchOne();
+    }
+
+    public List<PriceCategoryRecord> findAll() {
+        return dsl.selectFrom(PriceCategory.PRICE_CATEGORY).fetch();
+    }
+}
+
