477f54a794aa5c3e71dbb3496bf100b996055c06
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-14 16:59:11 +0300

    Добавил RestaurantKitchenTypeService

---
+++ data/src/main/java/dev/tishenko/restobot/data/service/RestaurantKitchenTypeService.java
@@ -0,0 +1,30 @@
+package dev.tishenko.restobot.data.service;
+
+import java.util.List;
+
+import org.example.jooq.generated.tables.records.RestaurantKitchenTypeRecord;
+import org.springframework.stereotype.Service;
+
+import dev.tishenko.restobot.data.repository.RestaurantKitchenTypeRepository;
+
+@Service
+public class RestaurantKitchenTypeService {
+  
+  private final RestaurantKitchenTypeRepository repo;
+
+  public RestaurantKitchenTypeService(RestaurantKitchenTypeRepository repo) {
+    this.repo = repo;
+  } 
+
+  public void addKitchen(int restaurantId, int kitchenTypeId) {
+    repo.addKitchen(restaurantId, kitchenTypeId);
+  }
+
+  public void removeKitchen(int restaurantId, int kitchenTypeId) {
+    repo.removeKitchen(restaurantId, kitchenTypeId);
+  }
+
+  public List<RestaurantKitchenTypeRecord> getAllByRestaurant(int restaurantId) {
+    return repo.getAllByRestaurant(restaurantId);
+  }
+}
