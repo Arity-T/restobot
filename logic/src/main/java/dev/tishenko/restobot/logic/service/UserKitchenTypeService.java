af4c4bfd7efa99491cd29f8f609bf11ed34785ea
Vladislav Gaar <gaarvlad@gmail.com>
2025-05-14 16:50:12 +0300

    Поправил местами импорты в UserKitchenTypeService

--- logic/src/main/java/dev/tishenko/restobot/logic/service/UserKitchenTypeService.java
+++ logic/src/main/java/dev/tishenko/restobot/logic/service/UserKitchenTypeService.java
@@ -1,10 +1,11 @@
 package dev.tishenko.restobot.logic.service;
 
-import dev.tishenko.restobot.logic.repository.UserKitchenTypeRepository;
+import java.util.List;
+
 import org.example.jooq.generated.tables.records.UserKitchenTypeRecord;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import dev.tishenko.restobot.logic.repository.UserKitchenTypeRepository;
 
 @Service
 public class UserKitchenTypeService {
