c1160c0bd105e1f843a248bd54a77049503069ce
DmitriyGubkovskiy <gubkovskij.dv@edu.spbstu.ru>
2025-05-16 23:18:41 +0300

    Добавил пример реализации FavoriteListDAO

---
+++ telegram/src/main/java/dev/tishenko/restobot/telegram/example/impl/ExampleFavoriteListDAO.java
@@ -0,0 +1,36 @@
+package dev.tishenko.restobot.telegram.example.impl;
+
+import dev.tishenko.restobot.telegram.services.FavoriteListDAO;
+import dev.tishenko.restobot.telegram.services.RestaurantCardDTO;
+import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ExampleFavoriteListDAO implements FavoriteListDAO {
+
+    private List<Map<RestaurantCardDTO, Boolean>> favouriteList = new ArrayList<>();
+
+    @Override
+    public void addRestaurantCardToFavoriteList(long chatId, int tripadvisorId) {
+
+    }
+
+    @Override
+    public void removeRestaurantCardToFavoriteList(long chatId, int tripadvisorId) {
+
+    }
+
+    @Override
+    public void setVisitedStatus(long chatId, int tripadvisorId, boolean isVisited) {
+
+    }
+
+    @Override
+    public List<Map<RestaurantCardDTO, Boolean>> getFavouriteList(long chatId) {
+        return favouriteList;
+    }
+}
