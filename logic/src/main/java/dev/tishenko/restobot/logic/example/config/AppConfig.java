5145ce37d16e760aa5cd8fd492562acad9f250b2
Arity-T <tish.mainmail@gmail.com>
2025-05-17 17:43:29 +0300

    Почему-то не подхватывает переменные окружения спринг, временно вынес в .properties файл

--- logic/src/main/java/dev/tishenko/restobot/logic/example/config/AppConfig.java
+++ logic/src/main/java/dev/tishenko/restobot/logic/example/config/AppConfig.java
@@ -1,11 +1,28 @@
 package dev.tishenko.restobot.logic.example.config;
 
-import dev.tishenko.restobot.api.example.ApiAppConfigExample;
+import dev.tishenko.restobot.api.RestobotApiConfig;
 import dev.tishenko.restobot.logic.config.LogicConfig;
 import dev.tishenko.restobot.telegram.config.BotFactoryConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.WebHandler;
 
 @Configuration
-@Import({ApiAppConfigExample.class, LogicConfig.class, BotFactoryConfig.class})
-public class AppConfig {}
+@Import({RestobotApiConfig.class, LogicConfig.class, BotFactoryConfig.class})
+@PropertySource("classpath:application.properties")
+public class AppConfig {
+    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+
+    @Bean
+    public WebHandler webHandler(RouterFunction<ServerResponse> restobotRoutes) {
+        logger.info("Configuring web handler with Restobot API routes");
+        return RouterFunctions.toWebHandler(restobotRoutes);
+    }
+}
