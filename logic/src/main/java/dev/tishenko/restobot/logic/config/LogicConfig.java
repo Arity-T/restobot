dc84eef32ccfe0d7eab1ab8200e4c3b9efcb020c
Arity-T <tish.mainmail@gmail.com>
2025-05-18 15:31:44 +0300

    Удалил LogicJooqConfig и перенёс бин DSLContext в LogicConfig

--- logic/src/main/java/dev/tishenko/restobot/logic/config/LogicConfig.java
+++ logic/src/main/java/dev/tishenko/restobot/logic/config/LogicConfig.java
@@ -1,8 +1,18 @@
 package dev.tishenko.restobot.logic.config;
 
+import javax.sql.DataSource;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("dev.tishenko.restobot.logic")
-public class LogicConfig {}
+public class LogicConfig {
+    @Bean
+    public DSLContext dslContext(DataSource dataSource) {
+        return DSL.using(dataSource, SQLDialect.POSTGRES);
+    }
+}
