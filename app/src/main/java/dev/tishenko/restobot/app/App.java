de4254bb63bbec8ac4724160ac5292aaf86dd846
Arity-T <tish.mainmail@gmail.com>
2025-05-18 20:57:15 +0300

    Перенёс пример из logic в отдельный проект

---
+++ app/src/main/java/dev/tishenko/restobot/app/App.java
@@ -0,0 +1,38 @@
+package dev.tishenko.restobot.app;
+
+import dev.tishenko.restobot.logic.example.config.AppConfig;
+import dev.tishenko.restobot.telegram.BotFacade;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import reactor.netty.http.server.HttpServer;
+
+public class App {
+    public static void main(String[] args) {
+        // Создаем контекст Spring
+        var context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Запускаем API сервер в отдельном потоке
+        var apiThread =
+                new Thread(
+                        () -> {
+                            var httpHandler =
+                                    WebHttpHandlerBuilder.applicationContext(context).build();
+                            var adapter = new ReactorHttpHandlerAdapter(httpHandler);
+
+                            HttpServer.create()
+                                    .host("localhost")
+                                    .port(8089)
+                                    .handle(adapter)
+                                    .bindNow()
+                                    .onDispose()
+                                    .block();
+                        });
+        apiThread.start();
+
+        // Запускаем Telegram бота в основном потоке
+        BotFacade telegramApp = context.getBean(BotFacade.class);
+        // Бот запускается автоматически через LongPollingUpdateConsumer
+        telegramApp.start();
+    }
+}
